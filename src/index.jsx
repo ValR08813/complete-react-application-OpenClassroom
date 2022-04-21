e1bf09950dab8c045ffec8a539e1464fd1f7067d
ValR08813 <valentin.deschins@gmail.com>
2022-04-21 22:43:12 +0200

    surveylinksdone

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
 import Error from './components/Error'
+import Freelances from './pages/Freelances'
+import Results from './pages/Results'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,6 +18,12 @@ ReactDOM.render(
         </Route>
         <Route path="/survey/:questionNumber">
           <Survey />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route path="/results">
+          <Results />
         </Route>
         <Route>
           <Error />

fe63ec489b1b08601fd17b18f7b4f01f54fda61f
ValR08813 <valentin.deschins@gmail.com>
2022-04-21 21:52:24 +0200

    header done

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/survey">
+        <Survey />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
